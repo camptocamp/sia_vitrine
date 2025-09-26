bfe06e09710fdee408c1995d975190e44fd9efcd
Antoine Abt <antoine@abt.im>
2025-09-26 12:02:44 +0200

    Add licence

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<!--
+  This file is part of SIA 62.
+  SIA 62 is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+  SIA 62 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+  You should have received a copy of the GNU Affero General Public License along with SIA 62. If not, see <https://www.gnu.org/licenses/>.
+-->
+
 <script setup lang="ts"></script>
 
 <template>
